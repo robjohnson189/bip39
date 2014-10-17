794e5465a39c75260e3f790a0afc17952c5a762a
Robert Johnson <sk8boy189@gmail.com>
2014-10-16 21:44:07 -0500

    adding modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-var gulp = require('gulp');
+var 	gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename');
 
-gulp.task('default', function(){
-	console.log("hi");
+
+gulp.task('default', ['compress']);
+
+gulp.task('compress', function() {
+	gulp.src(['src/js/jquery.min.js','src/js/bootstrap.min.js','src/js/bitcoinjs-1-0-0-.js',
+		'src/js/sjcl-bip39.js','src/js/wordlist_english.js','src/js/jsbip39.js', 'src/js/index.js'])
+		.pipe(concat('concat.js'))
+		.pipe(gulp.dest('dist'))
+		.pipe(uglify())
+		.pipe(rename('uglify.js'))
+		.pipe(gulp.dest('dist'))
 });
